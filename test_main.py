13b7c8f165cec7b97b7adda18b40d97b7a5a67fb
jeremymtan <jeremytan@Jeremys-MacBook-Pro.local>
2023-09-21 14:51:01 -0400

    fix 5

--- test_main.py
+++ test_main.py
@@ -9,7 +9,7 @@ from main import os_and_sys_version
 def test_os_and_sys_version():
     """Function calling os_and_sys_version"""
     python_version, os_name = os_and_sys_version()
-    python_version_check = ["3.7", "3.8", "3.9"]
+    python_version_check = ["3.7", "3.8", "3.9", "3.10", "3.11"]
     os_name_check = ["Windows", "Linux"]
     print(python_version, os_name)
     assert python_version in python_version_check
