839a07a5c5e0a4534d0f551f1edc4febe1332fcc
jeremymtan <jeremytan@Jeremys-MacBook-Pro.local>
2023-09-21 16:03:47 -0400

    update test cases

--- test_main.py
+++ test_main.py
@@ -11,9 +11,10 @@ def test_os_and_sys_version():
     python_version, os_name = os_and_sys_version()
     python_version_check = ["3.7", "3.8", "3.9", "3.10", "3.11"]
     os_name_check = ["Windows", "Linux"]
-    print(python_version, os_name)
     assert python_version in python_version_check
-    assert os_name in os_name_check
+    # if environment has an OS installed
+    if os_name is not None:
+        assert os_name in os_name_check
 
 
 if __name__ == "__main__":
